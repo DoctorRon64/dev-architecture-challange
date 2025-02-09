b18d24be048f4a867fdb1501ad6c419dc843a9f4
DrRon <DoctorRon64@users.noreply.github.com>
2025-02-09 14:28:05 +0100

    Eventmanager update

--- Challange/Assets/Script/EventManager.cs
+++ Challange/Assets/Script/EventManager.cs
@@ -9,25 +9,55 @@ public enum EventType
 
 public static class EventManager
 {
-    private static Dictionary<EventType, System.Action> eventDictionary = new Dictionary<EventType, System.Action>();
+    private Dictionary<EventType, Delegate> eventDictionary = new Dictionary<EventType, Delegate>();
 
-    public static void AddListener(EventType type, System.Action action)
+    public void AddListener<T>(EventType type, Action<T> action)
     {
         if (!eventDictionary.ContainsKey(type))
         {
             eventDictionary.Add(type, null);
         }
-        eventDictionary[type] += action;
+        eventDictionary[type] = (Action<T>)eventDictionary[type] + action;
     }
 
-    public static void RemoveListener(EventType type, System.Action action)
+    public void RemoveListener<T>(EventType type, Action<T> action)
     {
-        if (eventDictionary.ContainsKey(type) && eventDictionary[type] != null) { }
-        eventDictionary[type] -= action;
+        if (eventDictionary.ContainsKey(type) && eventDictionary[type] != null)
+        {
+            eventDictionary[type] = (Action<T>)eventDictionary[type] - action;
+        }
     }
 
-    public static void InvokeEvent(EventType type, System.Action action)
+    public void InvokeEvent<T>(EventType type, T parameter)
     {
-        eventDictionary[type]?.Invoke();
+        if (eventDictionary.ContainsKey(type) && eventDictionary[type] != null)
+        {
+            ((Action<T>)eventDictionary[type])?.Invoke(parameter);
+        }
+    }
+
+    public class EventManagerParamaterless
+    {
+        private Dictionary<EventType, System.Action> eventDictionary = new Dictionary<EventType, System.Action>();
+
+        public void AddListener(EventType type, System.Action action)
+        {
+            if (!eventDictionary.ContainsKey(type))
+            {
+                eventDictionary.Add(type, null);
+            }
+            eventDictionary[type] += action;
+        }
+
+        public void RemoveListener(EventType type, System.Action action)
+        {
+            if (eventDictionary.ContainsKey(type) && eventDictionary[type] != null) { }
+            eventDictionary[type] -= action;
+        }
+
+        public void InvokeEvent(EventType type, System.Action action)
+        {
+            eventDictionary[type]?.Invoke();
+        }
     }
 }
